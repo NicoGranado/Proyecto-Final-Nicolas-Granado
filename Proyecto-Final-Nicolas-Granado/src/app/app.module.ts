58e4cb5be89c0307bc0e8a7f7adcbd3f58d8576c
Nicolás <nicogranado18@gmail.com>
2022-08-14 05:30:38 -0300

    Banner y demás

--- Proyecto-Final-Nicolas-Granado/src/app/app.module.ts
+++ Proyecto-Final-Nicolas-Granado/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoArgComponent } from './componentes/logo-arg/logo-arg.component';
+import { SocialComponent } from './componentes/social/social.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
