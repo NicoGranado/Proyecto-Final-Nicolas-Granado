2c67bade88dd14ebf29366d34f28d584fdab5283
Nicolás <nicogranado18@gmail.com>
2022-08-14 05:44:18 -0300

    Acerca de

--- Proyecto-Final-Nicolas-Granado/src/app/app.module.ts
+++ Proyecto-Final-Nicolas-Granado/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './componentes/header/header.component';
 import { LogoArgComponent } from './componentes/logo-arg/logo-arg.component';
 import { SocialComponent } from './componentes/social/social.component';
 import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './componentes/banner/banner.component';
     HeaderComponent,
     LogoArgComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
